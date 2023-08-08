c6cac91d9399c2a2300df97c7a2c525931d2819b
Ryan Tan <65996005+ryn5@users.noreply.github.com>
2023-08-08 03:30:45 -0700

    Delete esports hub directory

--- esports hub/src/controller/Main.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-import ui.HomeScreen;
-
-public class Main {
-    public static void main(String[] args) {
-        new HomeScreen();
-    }
-}
--- esports hub/src/tabs/TeamMemberPanel.java
+++
@@ -1,4 +0,0 @@
-package tabs;
-
-public class TeamMemberPanel {
-}
